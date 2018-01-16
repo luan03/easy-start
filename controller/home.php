78d95174a99b777358ede18cf5f4ea3f528991b3
Luan Fagundes <luan03@msn.com>
2018-01-16 20:35:23 +1000

    twig installation

---
+++ controller/home.php
@@ -0,0 +1,7 @@
+<?php
+
+echo $twig->render('index.html.twig', array(
+    'Home' => array(
+        'title' => "Easy start template =)",
+    ),
+));
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once './vendor/autoload.php';
+
+//load templates
+$loader = new Twig_Loader_Filesystem('views');
+
+//set cache directory
+$twig = new Twig_Environment($loader, array(
+    'cache' => false,
+    'debug' => true,
+    'auto_reload' => true,
+));
+
+/**
+ * Home page
+ */
+require_once './controller/home.php';
